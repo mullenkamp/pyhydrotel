9874a22e2ef14226f209335460087fc3b6271106
MikeK <mgkittridge@gmail.com>
2018-08-17 15:30:10 +1200

    update to functions

--- pyhydrotel/core.py
+++ pyhydrotel/core.py
@@ -24,7 +24,7 @@ sites_tab = 'Sites'
 
 data_col = ['Point', 'DT', 'SampleValue']
 points_col = ['Point', 'Object']
-objects_col = ['Object', 'Site', 'ObjectVariant', 'Name']
+objects_col = ['Object', 'Site', 'ObjectVariant', 'Name', 'ExtSysID']
 mtypes_col = ['ObjectVariant', 'Name']
 sites_col = ['Site', 'Name', 'ExtSysId']
 
@@ -32,6 +32,8 @@ sites_col = ['Site', 'Name', 'ExtSysId']
 def get_mtypes(server, database):
     """
     Function to return a Series of measurement types that can be passed to get_sites_mtypes and get_ts_data. Returns with a count of the frequency the values exist in the database and is sorted by the count.
+
+    Remember, SQL is not case sensitive. The MTypes returned will have different cases, but these differences do not matter for the other functions.
 
     Parameters
     ----------
@@ -86,38 +88,58 @@ def get_sites_mtypes(server, database, mtypes, sites=None):
     ## Extract hydrotel site numbers for all ECan sites
     sites1 = rd_sql(server, database, sites_tab, sites_col)
     sites1['ExtSysId'] = sites1['ExtSysId'].str.strip()
-    sites1.rename(columns={'ExtSysId': 'ExtSysID'}, inplace=True)
-    ob1 = rd_sql(server, database, objects_tab, ['Site', 'ExtSysID']).dropna()
+    sites1 = sites1[sites1.ExtSysId != '']
+#    sites1.rename(columns={'ExtSysId': 'ExtSysID'}, inplace=True)
 
     # GW
-    names_len_bool = sites1.Name.str.upper().str.contains('[A-Z]+\d+/\d+')
+    names_len_bool = sites1.Name.str.upper().str.match('[A-Z]+\d+/\d+')
     gw_sites = sites1[names_len_bool].copy()
-    gw_sites.ExtSysID = gw_sites.Name.str.findall('[A-Z]+\d+/\d+').apply(lambda x: x[0])
+    gw_sites.ExtSysId = gw_sites.Name.str.findall('[A-Z]+\d+/\d+').apply(lambda x: x[0])
 #    gw_sites['MType'] = 'gwl'
 
     # Others
-    sites2 = sites1[sites1.ExtSysID.str.contains('\d+', na=False)].drop('Name', axis=1)
-    sites3 = ob1[ob1.ExtSysID.str.contains('\d+', na=False)]
+    sites2 = sites1[sites1.ExtSysId.str.match('\d+', na=False)].drop('Name', axis=1)
+#    sites3 = ob1[ob1.ExtSysID.str.contains('\d+', na=False)]
 
     # Combine and remove duplicates
-    sites_all = pd.concat([gw_sites.drop('Name', axis=1), sites2, sites3])
-    sites_all = sites_all.drop_duplicates('ExtSysID')
-
-    if isinstance(sites, list):
-        sites_all = sites_all[sites_all.ExtSysID.isin(sites)]
+    sites3 = pd.concat([gw_sites.drop('Name', axis=1), sites2])
+    sites3 = sites3.drop_duplicates('ExtSysId')
 
     ## objects
-    objects1 = rd_sql(server, database, objects_tab, objects_col, {'Site': sites_all.Site.tolist(), 'Name': mtypes})
+    objects1 = rd_sql(server, database, objects_tab, objects_col, {'Name': mtypes})
+    objects1.ExtSysID = objects1.ExtSysID.str.strip()
+    objects1.loc[objects1.ExtSysID == '', 'ExtSysID'] = None
 
-    # Join Sites to Objects
-    sites_ob1 = pd.merge(sites_all, objects1, on='Site')
+    ## Combine objects with sites
+    sites_ob1 = pd.merge(objects1, sites3, on='Site', how='left')
+    sites_ob1.loc[sites_ob1.ExtSysID.isnull(), 'ExtSysID'] = sites_ob1.loc[sites_ob1.ExtSysID.isnull(), 'ExtSysId']
+    sites_ob1 = sites_ob1.dropna(subset=['ExtSysID']).drop('ExtSysId', axis=1)
+
+    if isinstance(sites, list):
+        sites_ob1 = sites_ob1[sites_ob1.ExtSysID.isin(sites)]
+
     sites_ob1.Name = sites_ob1.Name.str.lower()
     sites_ob1.rename(columns={'Name': 'MType'}, inplace=True)
-#    sites_ob1.loc[sites_ob1.MType.isnull() & (sites_ob1.Name == 'water level'), 'MType'] = 'swl'
-#    sites_ob1.loc[sites_ob1.Name == 'flow', 'MType'] = 'flow'
-#    sites_ob1.loc[sites_ob1.Name == 'rainfall', 'MType'] = 'precip'
 
-    return sites_ob1
+    ## Import object/point data
+    point_val = rd_sql(server, database, points_tab, points_col, where_col='Object', where_val=sites_ob1.Object.tolist())
+
+    # Merge
+    site_point = pd.merge(sites_ob1, point_val, on='Object')
+
+    ## Get from and to dates
+    sql_stmt = """select Point, min(DT) as FromDate, max(DT) as ToDate
+                from Hydrotel.dbo.Samples
+                where Point in ({sites})
+                group by Point""".format(sites=str(site_point.Point.tolist())[1:-1])
+    min_max_point = rd_sql(server, database, stmt=sql_stmt)
+
+    ## Combine all together
+    site_summ = pd.merge(site_point, min_max_point, on='Point', how='left')
+    site_summ.rename(columns={'ExtSysID': 'ExtSiteID'}, inplace=True)
+    site_summ.set_index(['ExtSiteID', 'MType'], inplace=True)
+
+    return site_summ
 
 
 def get_ts_data(server, database, mtypes, sites, from_date=None, to_date=None, resample_code='D', period=1, val_round=3, min_count=None, pivot=False):
@@ -153,15 +175,17 @@ def get_ts_data(server, database, mtypes, sites, from_date=None, to_date=None, r
         A MultiIndex Pandas Series if pivot is False and a DataFrame if True
     """
     ### Import data and select the correct sites
-    object_val = get_sites_mtypes(server, database, mtypes, sites)
+    site_point = get_sites_mtypes(server, database, mtypes, sites).reset_index()
 
-    ### Import object/point data
-    point_val = rd_sql(server, database, points_tab, points_col, where_col='Object', where_val=object_val.Object.tolist())
-
-    ### Merge
-    site_point = pd.merge(object_val[['ExtSysID', 'MType', 'Object']], point_val, on='Object').drop('Object', axis=1)
+    ### Select rows within time period
+    if isinstance(from_date, str):
+        site_point = site_point[site_point.ToDate > from_date]
+    if isinstance(to_date, str):
+        site_point = site_point[site_point.FromDate < to_date]
 
     ### Pull out the ts data
+    site_point1 = site_point[['ExtSiteID', 'MType', 'Point']].copy()
+
     tsdata_list = []
 
     mtypes1 = site_point.MType.unique()
@@ -171,13 +195,13 @@ def get_ts_data(server, database, mtypes, sites, from_date=None, to_date=None, r
             res_val = resample_dict[m]
         else:
             res_val = 'mean'
-        sel = site_point[site_point.MType == m]
+        sel = site_point1[site_point1.MType == m]
         points = sel.Point.astype(int).tolist()
 
         data1 = rd_sql_ts(server, database, data_tab, 'Point', 'DT', 'SampleValue', resample_code, period, res_val, val_round, {'Point': points}, from_date=from_date, to_date=to_date, min_count=min_count).reset_index()
 
         data1.rename(columns={'DT': 'DateTime', 'SampleValue': 'Value'}, inplace=True)
-        data2 = pd.merge(sel, data1, on='Point').drop('Point', axis=1).set_index(['ExtSysID', 'MType', 'DateTime']).Value
+        data2 = pd.merge(sel, data1, on='Point').drop('Point', axis=1).set_index(['ExtSiteID', 'MType', 'DateTime']).Value
         tsdata_list.append(data2)
 
     tsdata = pd.concat(tsdata_list)
